d22cb6e6a88add5e154c2f1faafc1772e964772f
vortacs <jtoscanifield@ucmerced.edu>
2021-08-27 14:05:47 -0700

    file pre-processing and duplicate sra checking

--- intensiphy.py
+++ intensiphy.py
@@ -15,11 +15,14 @@ def parse_args():
     parser.add_argument('--update_method')
     return parser.parse_args()
 
-def read_csv(csv_file):
+def read_csv_file(csv_file):
     """Reads the accession file provided by the users."""
     csv = pd.read_csv(csv_file)
+    
+    just_accessions = csv["Run"]
+    # print(just_accessions)
 
-    return csv
+    return just_accessions
 
 def calulate_cores(set_cores):
     """Organizes and calulates the cores for use with Extensiphy."""
@@ -41,8 +44,41 @@ def calulate_cores(set_cores):
     output.append(int(runs))
     output.append(int(cores_per_run))
 
+    # test to make sure we get a list of length 2
+    # that contains 2 ints
+    assert type(output) == list
+    assert len(output) == 2
+    for num in output:
+        assert type(num) == int
+
     return output
-        
+    
+def check_duplicate_accesions(accession_db, fasta_names):
+    """Checks the alignment file vs the accesion list
+    and produces a list of accessions that are already in the alignment."""
+    duplicate_sra_runs = []
+    
+    for value in accession_db:
+        if value in fasta_names:
+            assert type(value) == str
+            assert len(value) > 1
+            duplicate_sra_runs.append(value)
+
+    return duplicate_sra_runs
+
+def read_fasta_names(align):
+    """Reads the names of the sequences from the fasta file."""
+    names = []
+    with open(align, 'r') as fasta_file:
+        for line in fasta_file:
+            if line.startswith('>'):
+                seq_name = line.strip(">").strip('\n')
+                assert len(seq_name) > 1
+                assert type(seq_name) == str
+                names.append(seq_name)
+    
+    return names
+
 
 
 def main():
@@ -50,8 +86,16 @@ def main():
 
     # calculate the core organization to pass to Extensiphy
     get_cores = calulate_cores(args.cores)
-    print(get_cores)
+    # print(get_cores)
 
+    # read_file = read_csv_file(args.accession_csv)
+    read_fasta = read_fasta_names(args.align_file)
+
+    #read list of accessions
+    read_accessions = read_csv_file(args.accession_csv)
+
+    #Check list of run SRA numbers vs the sequences already in the alignment to prevent duplicates.
+    check_dupes = check_duplicate_accesions(read_accessions, read_fasta)
     
 
 if __name__ == '__main__':
