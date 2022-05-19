955b156caf844a654060124667f5df2690e3c1fe
vortacs <jtoscanifield@ucmerced.edu>
2022-05-18 21:32:28 -0700

    program to output a newick tree from a nexus input

---
+++ modules/nexus_to_newick.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import pathlib
+import shutil
+from numpy.lib.shape_base import split
+import pandas as pd
+import subprocess
+import datetime
+import dateutil
+import dendropy
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog='nexus to newick', \
+        description='convert a tree in nexus format to a tree in newick format')
+    parser.add_argument('--tree', help='Newick phylogeny file.')
+    parser.add_argument('--output_file', default='converted_tree.tre', help='newick phylogeny file.')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    # tree = dendropy.Tree.get(path=args.tree_file, schema="newick", preserve_underscores=True)
+
+    output = []
+
+    tree = dendropy.Tree.get(path=args.tree, schema="nexus", preserve_underscores=True)
+
+    print(tree)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
